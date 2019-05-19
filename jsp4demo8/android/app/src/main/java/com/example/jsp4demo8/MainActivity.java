23cb359ce77e30eea5fe4b66db889f22e048a6ec
RtyXmd <573850993@qq.com>
2019-05-19 18:14:24 +0800

    展开闭合案例 --init--

---
+++ jsp4demo8/android/app/src/main/java/com/example/jsp4demo8/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jsp4demo8;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
