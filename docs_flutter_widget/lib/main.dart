f9e3a613668c9f27668a66abf918046caab29a0e
jgsrty <573850993@qq.com>
2019-05-20 15:55:33 +0800

    根据用户输入改变widget

--- docs_flutter_widget/lib/main.dart
+++ docs_flutter_widget/lib/main.dart
@@ -1,5 +1,51 @@
 import 'package:flutter/material.dart';
 
+/** 根据用户输入改变widget */
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+  void _increment(){
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CounterIncrementor(onPressed: _increment,),
+        CounterDisplay(count:_counter)
+      ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+  final int count;
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count:$count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+  final VoidCallback onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child:Text('Increment')
+    );
+  }
+}
+
+
 /** 使用material组件 */
 void main(){
   runApp(MaterialApp(
@@ -27,7 +73,7 @@ class TutorialHome extends StatelessWidget {
       // body:Center(
       //   child:Text('hello,world.')
       // ),
-      body:MyButton(),
+      body:Counter(),
       floatingActionButton: FloatingActionButton(
         child:Icon(Icons.add),
         onPressed: (){},
@@ -36,28 +82,28 @@ class TutorialHome extends StatelessWidget {
   }
 }
 
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap:(){
-        print('myBUtton was tapped');
-      },
-      child:Container(
-        height:36.0,
-        padding:const EdgeInsets.all(8.0),
-        margin:const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration:BoxDecoration(
-          borderRadius:BorderRadius.circular(5.0),
-          color:Colors.lightGreen
-        ),
-        child:Center(
-          child:Text('Engage')
-        )
-      )
-    );
-  }
-}
+// class MyButton extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap:(){
+//         print('myBUtton was tapped');
+//       },
+//       child:Container(
+//         height:36.0,
+//         padding:const EdgeInsets.all(8.0),
+//         margin:const EdgeInsets.symmetric(horizontal: 8.0),
+//         decoration:BoxDecoration(
+//           borderRadius:BorderRadius.circular(5.0),
+//           color:Colors.lightGreen
+//         ),
+//         child:Center(
+//           child:Text('Engage')
+//         )
+//       )
+//     );
+//   }
+// }
 
 
 
