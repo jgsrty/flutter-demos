1f9bb8aed99c6f9a0495cb56984820d8ca7da189
jgsrty <573850993@qq.com>
2019-05-20 14:43:23 +0800

    使用widget组件

---
+++ docs_flutter_widget/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+/** 使用material组件 */
+
+
+/** 使用widget组件 */
+// void main(){
+//   runApp(MaterialApp(
+//     title:'my app',
+//     home:MyScaffold()
+//   ));
+// }
+
+// class MyScaffold extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       child:Column(
+//         children: <Widget>[
+//           MyAppBar(
+//             title:Text('example title',style:Theme.of(context).primaryTextTheme.title),
+//           ),
+//           Expanded(
+//             child:Center(
+//               child:Text('hello, world~')
+//             )
+//           )
+//         ],
+//       )
+//     );
+//   }
+// }
+
+// class MyAppBar extends StatelessWidget {
+//   MyAppBar({this.title});
+//   final Widget title;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height:56.0,
+//       padding:const EdgeInsets.symmetric(horizontal:8.0),
+//       decoration:new BoxDecoration(color:Colors.blue),
+//       child:Row(
+//         children: <Widget>[
+//           IconButton(
+//             icon:Icon(Icons.menu),
+//             tooltip: 'navigation menu',
+//             onPressed: (){},
+//           ),
+//           Expanded(child: title,),
+//           IconButton(
+//             icon:Icon(Icons.search),
+//             tooltip: 'search',
+//             onPressed: (){},
+//           )
+//         ],
+//         )
+//     );
+//   }
+// }
