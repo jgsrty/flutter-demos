2636d4b7ce9811ce27b8120e3396d6579fa55458
RtyXmd <573850993@qq.com>
2019-07-25 16:02:55 +0800

    StatelessWidget

--- juejin_brochure/flutter_demo_2019_07_25/lib/main.dart
+++ juejin_brochure/flutter_demo_2019_07_25/lib/main.dart
@@ -1,40 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp('myApp'));
+void main() => runApp(NewApp('newApp'));
 
-class MyApp extends StatefulWidget {
-  String content;
+class NewApp extends StatelessWidget {
 
-  MyApp(this.content);
+  final String content;
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  bool isShowText = true;
-
-  void increment(){
-    setState((){
-      widget.content += 'd';
-    });
-  }
+  NewApp(this.content);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title:'app title',
-      theme:ThemeData(
-        primarySwatch:Colors.blue
-      ),
+      title:'i am title',
       home:Scaffold(
-        appBar: AppBar(title:Text('StateFulWeight & state')),
         body:Center(
-          child:GestureDetector(
-            child:isShowText?Text(widget.content):Text('empty'),
-            onTap:increment
-          ),
+          child:Text(content)
         )
       )
     );
